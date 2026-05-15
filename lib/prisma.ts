610bb826aad9c7b0be11b42d3bfa72dacbe43095
AlfinurFitraWijayaR <alfinurfitrawijaya13@gmail.com>
2026-05-15 10:04:12 +0700

    update prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,33 @@
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// export const runtime = "nodejs";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient({
+//     adapter: new PrismaPg({
+//       connectionString: process.env.DATABASE_URL!,
+//     }),
+//   });
+// };
+
+// export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
+
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 export const runtime = "nodejs";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    adapter: new PrismaPg({
-      connectionString: process.env.DATABASE_URL!,
-    }),
-  });
-};
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
